dc83acf8d4983ca4ce152c255290febae9140e2b
Mark Gardner <m15399@gmail.com>
2015-09-17 21:56:17 -0700

    Thumbnail image size is set better, cleaner HTML

--- setup.js
+++ setup.js
@@ -2,10 +2,10 @@
 (function(){
 
 	// Fill the 'nav' div, if found
-	var nav = document.getElementById('nav');
+	var nav = document.getElementsByTagName('nav')[0];
 	if(nav){
 		nav.innerHTML = '<ul>\
-			<a href="index.html"><li>Home</li></a>\
+			<a href="index.html"><li class="active">Home</li></a>\
 			<a href="index.html"><li>Projects</li></a>\
 			<a href="index.html"><li>About Me</li></a>\
 			<a href="index.html"><li>Contact</li></a>\
