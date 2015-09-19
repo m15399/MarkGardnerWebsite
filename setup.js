8b61d7128b5c585cc952b09a5943041a61b8f237
Mark Gardner <m15399@gmail.com>
2015-09-19 14:08:38 -0700

    Simplified markup for projectThumbs (using custom tags and JS)

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 
 (function(){
 
-	// Fill the 'nav' div, if found
+	// Fill the 'nav' section, if found
 	var nav = document.getElementsByTagName('nav')[0];
 	if(nav){
 		nav.innerHTML = '<ul>\
@@ -10,6 +10,37 @@
 			<a href="index.html"><li>About Me</li></a>\
 			<a href="index.html"><li>Contact</li></a>\
 			</ul>';
+	}
+
+	// Format projectThumbs
+	var thumbs = document.getElementsByTagName('projectThumb');
+	for(var i = 0; i < thumbs.length; i++){
+		var thumb = thumbs[i];
+
+		// Set background image
+		var image = thumb.getAttribute('image');
+		if(image)
+			thumb.style.backgroundImage = 'url(' + image + ')';
+
+		// Put the text in a 'hoverOverlay' div
+		// We're making something like:
+		//   <projectThumb>
+		//     <div class='hoverOverlay'>
+		//       <div class='paddedText'>Description</div
+		//     </div>
+		//   </projectThumb
+		var text = thumb.innerText;
+		thumb.innerHTML = '';
+
+		var overlay = document.createElement('div');
+		overlay.className = 'hoverOverlay';
+
+		var description = document.createElement('div');
+		description.className = 'paddedText';
+		description.innerText = text;
+
+		overlay.appendChild(description);
+		thumb.appendChild(overlay);
 	}
 
 }());
